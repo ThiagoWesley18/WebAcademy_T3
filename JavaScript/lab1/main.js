3006317001caf88ebe85401af48f366073b4ab79
ThiagoWesley18 <tw180896@gmail.com>
2024-02-20 18:21:03 -0400

    lab1

--- JavaScript/lab1/main.js
+++ JavaScript/lab1/main.js
@@ -7,10 +7,6 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-//const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
-//const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
-//const insertY = ["the soup kitchen", "Disneyland", "the White House"];
-//const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 const storyText = "A Enterprise encontra uma névoa musical que :insertx:. Data tenta decifrar a linguagem musical, enquanto Spock sente uma :inserty: com a melodia. A Dra. Crusher observa diferentes reações entre os membros da tripulação. A melodia é revelada como uma mensagem de :inserty: de uma civilização alienígena. A Enterprise estabelece contato :insertz:";
 const insertX = ["emana uma melodia misteriosa", "encanta a tripulação com sua beleza", "se revela como uma arma alienígena"];
 const insertY = ["paz", "raiva", "tristeza"];
