4873fc97d725e0c5418089bb6440dce5397f8c9b
ThiagoWesley18 <tw180896@gmail.com>
2024-03-17 11:46:46 -0400

    exercicio node1 completo

--- node/node1/src/main.js
+++ node/node1/src/main.js
@@ -1,25 +1,47 @@
 
-const fs = require('fs');
-const http = require('http');
+import fs from 'fs';
+import http from "http";
+import {createLink , voltar} from './utils.js';
+import dotenv from "dotenv";
 
-const server = http.createServer((_, res) => {
-    
-    fs.readdir(process.argv[2], (err, files) => {
-        if (err) {
-            console.error('Ocorreu um erro ao ler o diretório:', err);
-            return;
-        }else{
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            files.forEach(file => {
-                res.write(`${file}<br>`);
-                console.log(file);
-            });
-        }
-        res.end();
-    });  
+/* configura process.env para receber os valores de configurações do arquivo que é apontado pela variavel 
+ambiente NODE_ENV*/
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` })
+const PORT = process.env.PORT;
+
+// cria o servidor
+const server = http.createServer((req, res) => {
+
+    if(req.url === '/'){
+        // lê o diretório passado como argumento através do process.argv[2] - primeiro argumento
+        fs.readdir(process.argv[2], (err, files) => {
+            if (err) {
+                console.error('Ocorreu um erro ao ler o diretório:', err);
+                return;
+            }else{
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                files.forEach(file => {
+                    res.write(createLink(`${file}`));
+                });
+            }
+            res.end();
+        });  
+    }else{
+        res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+        fs.readFile(`./${req.url}`, (err, data) => {
+            if (err) {
+                console.error('Ocorreu um erro ao ler o arquivo:', err);
+                return;
+            }else{ 
+                res.write(voltar() +"<br>" + data);
+            }
+            res.end();
+        });
+    }  
 });
 
-server.listen(5000);
+// inicia o servidor na PORT definida
+server.listen(PORT);
 
 
 
---
+++ node/node1/src/utils.js
@@ -0,0 +1,4 @@
+
+export const createLink = (file) => `<a href="../arquivos/${file}">${file}</a><br>`; // para importar temos utilizar o mesmo nome entre chaves que foi exportado
+
+export const voltar = () => `<a href="/">Voltar</a><br>`; // para importar temos utilizar o mesmo nome entre chaves que foi exportado
