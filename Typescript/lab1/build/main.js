bf93a728bdad691c17f0b69cd9577fbcf244ebb6
ThiagoWesley18 <tw180896@gmail.com>
2024-03-29 17:10:21 -0400

    madificaçao nos parametros da classe row das linhas do grid

--- Typescript/lab1/build/main.js
+++ Typescript/lab1/build/main.js
@@ -3,6 +3,7 @@ let add = document.querySelector('#add');
 let remover = document.querySelector('#remover');
 let modificar = document.querySelector('#modificar');
 let paiLinha = document.querySelector('#paiLinha');
+// Tratamento do evento de Adição de lembrete
 add.addEventListener('click', () => {
     let linhas = document.getElementsByName('linha');
     let colunas = linhas.length > 0 ? linhas[linhas.length - 1].querySelectorAll('.col').length : 0;
@@ -25,31 +26,17 @@ add.addEventListener('click', () => {
     cardTextDataFim.innerHTML = "Data de fim : " + dataFim.value;
     cardTextDesc.innerHTML = `<p>Descrição:</p> <p style="border:3px solid black" >${desc.value}</p>`;
     // adicionando os atributos dos elementos
-    col.setAttribute('class', 'col');
+    col.setAttribute('class', 'col-md-4 col-sm-2');
     card.setAttribute('class', 'card');
     cardBody.setAttribute('class', 'card-body');
     cardTitle.setAttribute('class', 'card-title');
     cardTextDataInicio.setAttribute('class', 'card-text');
     cardTextDataFim.setAttribute('class', 'card-text');
     cardTextDesc.setAttribute('class', 'card-text');
-    // cria linha quando não tiver nenhuma linha, se não adiciona a coluna na linha
-    if (linhas.length == 0) {
+    // cria linha quando tiver 4 colunas, senão adiciona a coluna na mesma linha. E cria linha quando não tiver nenhuma linha, se não adiciona a coluna na linha    
+    if (colunas == 3 || linhas.length == 0) {
         let linha = document.createElement('div');
-        linha.setAttribute('class', 'row row-col-1 col-md-4 col-sm-2 g-3 w-100 h-100');
-        linha.setAttribute('name', 'linha');
-        paiLinha.appendChild(linha);
-        linha.appendChild(col);
-        col.appendChild(card);
-        card.appendChild(cardBody);
-        cardBody.appendChild(cardTitle);
-        cardBody.appendChild(cardTextDataInicio);
-        cardBody.appendChild(cardTextDataFim);
-        cardBody.appendChild(cardTextDesc);
-        // cria linha quando tiver 4 colunas    
-    }
-    else if (colunas == 4) {
-        let linha = document.createElement('div');
-        linha.setAttribute('class', 'row row-col-1 col-md-4 col-sm-2 g-3 w-100 h-100');
+        linha.setAttribute('class', 'row g-3 w-100 h-100');
         linha.setAttribute('name', 'linha');
         paiLinha.appendChild(linha);
         linha.appendChild(col);
@@ -70,6 +57,7 @@ add.addEventListener('click', () => {
         cardBody.appendChild(cardTextDesc);
     }
 });
+// tratamento do evento de remoção 
 remover.addEventListener('click', () => {
     // pegando todos os titulos dos cards
     let titulos = document.querySelectorAll('.card-title');
@@ -80,6 +68,7 @@ remover.addEventListener('click', () => {
     if (titulos.length > 0) {
         // removendo o card que tem o titulo igual ao valor do input
         titulos.forEach((linha) => {
+            // volta 3 elementos na arvore DOM para remover o card coluna
             if (linha.textContent == elemento.value) {
                 elementoRemover = linha.parentElement;
                 elementoRemover = elementoRemover.parentElement;
@@ -89,6 +78,7 @@ remover.addEventListener('click', () => {
         });
     }
 });
+// Tratamento do evento de Modificação de lembrete   
 modificar.addEventListener('click', () => {
     // pegando todos os titulos dos cards
     let titulos = document.querySelectorAll('.card-title');
--- Typescript/lab1/src/main.ts
+++ Typescript/lab1/src/main.ts
@@ -31,7 +31,7 @@ add.addEventListener('click', () => {
     cardTextDesc.innerHTML = `<p>Descrição:</p> <p style="border:3px solid black" >${desc.value}</p>`;
 
     // adicionando os atributos dos elementos
-    col.setAttribute('class', 'col');
+    col.setAttribute('class', 'col-md-4 col-sm-2');
     card.setAttribute('class', 'card');
     cardBody.setAttribute('class', 'card-body');
     cardTitle.setAttribute('class', 'card-title');
@@ -39,24 +39,10 @@ add.addEventListener('click', () => {
     cardTextDataFim.setAttribute('class', 'card-text');
     cardTextDesc.setAttribute('class', 'card-text');
 
-
-    // cria linha quando não tiver nenhuma linha, se não adiciona a coluna na linha
-    if(linhas.length == 0){
+    // cria linha quando tiver 4 colunas, senão adiciona a coluna na mesma linha. E cria linha quando não tiver nenhuma linha, se não adiciona a coluna na linha    
+    if(colunas == 3 || linhas.length == 0){
         let linha: HTMLDivElement = document.createElement('div');
-        linha.setAttribute('class', 'row row-col-1 col-md-4 col-sm-2 g-3 w-100 h-100');
-        linha.setAttribute('name', 'linha');
-        paiLinha.appendChild(linha);
-        linha.appendChild(col);
-        col.appendChild(card);
-        card.appendChild(cardBody);
-        cardBody.appendChild(cardTitle);
-        cardBody.appendChild(cardTextDataInicio);
-        cardBody.appendChild(cardTextDataFim);
-        cardBody.appendChild(cardTextDesc);
-    // cria linha quando tiver 4 colunas, senão adiciona a coluna na mesma linha    
-    }else if(colunas == 4){
-        let linha: HTMLDivElement = document.createElement('div');
-        linha.setAttribute('class', 'row row-col-1 col-md-4 col-sm-2 g-3 w-100 h-100');
+        linha.setAttribute('class', 'row g-3 w-100 h-100');
         linha.setAttribute('name', 'linha');
         paiLinha.appendChild(linha);
         linha.appendChild(col);
