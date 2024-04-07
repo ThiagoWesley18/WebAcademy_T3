c821d2f70c3da647d69f6c0a9aa2315a2a80a423
ThiagoWesley18 <tw180896@gmail.com>
2024-04-07 17:06:54 -0400

    egine handlebars

--- ModuloExpress/ExpTS/build/index.js
+++ ModuloExpress/ExpTS/build/index.js
@@ -8,6 +8,7 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const envalid_1 = __importDefault(require("./utils/envalid"));
 const logger_1 = __importDefault(require("./middleware/logger"));
 const router_1 = __importDefault(require("./router/router"));
+const express_handlebars_1 = require("express-handlebars");
 /*
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` }); // cria um arquivo .env.development ou .env.production
 //e alteramos package.json para "start": "NODE_ENV=development npx nodemon src/index.ts" e "start:prod": "NODE_ENV=production node src/index.ts"
@@ -16,9 +17,13 @@ dotenv_1.default.config(); // informamos se é developement ou production no arq
 (0, envalid_1.default)(); // valida se as variáveis de ambiente estão definidas
 const PORT = process.env.PORT;
 const app = (0, express_1.default)();
+// configuramos o handlebars
+app.engine("handlebars", (0, express_handlebars_1.engine)());
+app.set("view engine", "handlebars");
+app.set("views", `./views`);
 // criamos nosso middleware logger
 app.use((0, logger_1.default)("completo"));
-// como passar um pagina html para o cliente
+// como passar um pagina html estática para o cliente
 app.use("/html", express_1.default.static(`${__dirname}/../public/`));
 app.use("/css", express_1.default.static(`${__dirname}/../public/css`));
 app.use("/js", express_1.default.static(`${__dirname}/../public/js`));
--- ModuloExpress/ExpTS/src/index.ts
+++ ModuloExpress/ExpTS/src/index.ts
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import validateEnv from './utils/envalid';
 import logger from './middleware/logger';
 import  router  from './router/router';
+import { engine } from 'express-handlebars';
 
 /*
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` }); // cria um arquivo .env.development ou .env.production 
@@ -16,10 +17,15 @@ validateEnv(); // valida se as variáveis de ambiente estão definidas
 const PORT = process.env.PORT;
 const app = express();
 
+// configuramos o handlebars
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", `./views`);
+
 // criamos nosso middleware logger
 app.use(logger("completo"));
 
-// como passar um pagina html para o cliente
+// como passar um pagina html estática para o cliente
 app.use("/html", express.static(`${__dirname}/../public/`));
 app.use("/css", express.static(`${__dirname}/../public/css`));
 app.use("/js", express.static(`${__dirname}/../public/js`));
