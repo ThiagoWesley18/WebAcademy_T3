ce6c2b7103ffc6d7e2e7281cc7061b1d41000ece
ThiagoWesley18 <tw180896@gmail.com>
2024-04-28 12:47:19 -0400

    cookie

--- ExpAPI/build/resources/language/language.schemas.js
+++ ExpAPI/build/resources/language/language.schemas.js
@@ -5,6 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const joi_1 = __importDefault(require("joi"));
 const languageSchema = joi_1.default.object().keys({
-    lang: joi_1.default.valid('pt-BR', 'en-US').required(),
+    lang: joi_1.default.valid('pt-BR', 'en-US').required().messages({
+        "any.only": "O idioma deve ser 'pt-BR' ou 'en-US', o valor {#value} é inválido",
+    }),
 });
 exports.default = languageSchema;
--- ExpAPI/src/resources/language/language.schemas.ts
+++ ExpAPI/src/resources/language/language.schemas.ts
@@ -1,7 +1,9 @@
 import joi from 'joi';
 
 const languageSchema = joi.object().keys({
-    lang: joi.valid('pt-BR', 'en-US').required(),
+    lang: joi.valid('pt-BR', 'en-US').required().messages({
+        "any.only": "O idioma deve ser 'pt-BR' ou 'en-US', o valor {#value} é inválido",
+    }),
 });
 
 export default languageSchema;
